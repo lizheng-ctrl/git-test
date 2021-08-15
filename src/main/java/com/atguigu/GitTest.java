a0b60c06f9b9f497ce59e89948197818bcb11c69
ken <3087860936@qq.com>
2021-08-15 15:40:24 +0800

    third commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("此电脑");
         System.out.println("git.commit1");
+        System.out.println("git commit2");
     }
 }
