34122ded43c32a56c909e4e3b3bc12b2ea725581
ken <3087860936@qq.com>
2021-08-15 16:12:41 +0800

    master test commit2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,7 +4,7 @@ package com.atguigu;
  * */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("此电脑");
+        System.out.println("master commit test  hello");
         System.out.println("git.commit1");
         System.out.println("git commit2");
         System.out.println("git commit3");
